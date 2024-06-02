c74b23acfefbd5024d60c6183fe24981f41dda6e
Mohammed Hamza <211220035@nitdelhi.ac.in>
2024-06-02 23:07:00 +0530

    Error Validation Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,6 +11,7 @@ function Home () {
   ])
 
   const validateNumber = value => value.match(/^\d+(\.\d{1,3})?$/)
+
   const sgValue = semesters[semesters.length - 1].sgpa
 
   const isSgpaInvalid = useMemo(() => {
@@ -49,63 +50,68 @@ function Home () {
           <div className='flex items-center justify-center flex-col bg-primary-200 font-poppins'>
             <form>
               <div id='Sem'>
-                {semesters.map((sem, index) => (
-                  <div
-                    key={index}
-                    className='mt-4 flex w-full flex-wrap md:flex-nowrap gap-4'
-                  >
-                    <Input
-                      isReadOnly
-                      color='secondary'
-                      name='semester'
-                      size='lg'
-                      type='text'
-                      variant='bordered'
-                      label={`Semester No.`}
-                      value={index + 1}
-                    />
-                    <Input
-                      isRequired
-                      size='lg'
-                      name='sgpa'
-                      step={0.01}
-                      type='text'
-                      variant='bordered'
-                      label={`Semester G.P.A.`}
-                      isInvalid={isSgpaInvalid}
-                      color={isSgpaInvalid ? 'danger' : 'secondary'}
-                      errorMessage={
-                        isSgpaInvalid && 'SGPA must be a valid number'
-                      }
-                      value={sem.sgpa}
-                      onChange={e => handleSemChange(e, index)}
-                    />
-                    <Input
-                      isRequired
-                      size='lg'
-                      name='credits'
-                      type='text'
-                      variant='bordered'
-                      label={`Semester Credits`}
-                      isInvalid={isCreditInvalid}
-                      color={isCreditInvalid ? 'danger' : 'secondary'}
-                      errorMessage={
-                        isCreditInvalid && 'Credit must be a valid number'
-                      }
-                      value={sem.credits}
-                      onChange={e => handleSemChange(e, index)}
-                    />
-                    <Input
-                      isReadOnly
-                      size='lg'
-                      name='cgpa'
-                      type='number'
-                      label={`C.G.P.A.`}
-                      value={sem.cgpa}
-                      color={getCGPAColor(parseFloat(sem.cgpa))}
-                    />
-                  </div>
-                ))}
+                {semesters.map((sem, index) => {
+                  const isSgpaInvalid = sem.sgpa && !validateNumber(sem.sgpa)
+                  const isCreditInvalid =
+                    sem.credits && !validateNumber(sem.credits)
+                  return (
+                    <div
+                      key={index}
+                      className='mt-4 flex w-full flex-wrap md:flex-nowrap gap-4'
+                    >
+                      <Input
+                        isReadOnly
+                        color='secondary'
+                        name='semester'
+                        size='lg'
+                        type='text'
+                        variant='bordered'
+                        label={`Semester No.`}
+                        value={index + 1}
+                      />
+                      <Input
+                        isRequired
+                        size='lg'
+                        name='sgpa'
+                        step={0.01}
+                        type='text'
+                        variant='bordered'
+                        label={`Semester G.P.A.`}
+                        isInvalid={isSgpaInvalid}
+                        color={isSgpaInvalid ? 'danger' : 'secondary'}
+                        errorMessage={
+                          isSgpaInvalid && 'SGPA must be a valid number'
+                        }
+                        value={sem.sgpa}
+                        onChange={e => handleSemChange(e, index)}
+                      />
+                      <Input
+                        isRequired
+                        size='lg'
+                        name='credits'
+                        type='text'
+                        variant='bordered'
+                        label={`Semester Credits`}
+                        isInvalid={isCreditInvalid}
+                        color={isCreditInvalid ? 'danger' : 'secondary'}
+                        errorMessage={
+                          isCreditInvalid && 'Credit must be a valid number'
+                        }
+                        value={sem.credits}
+                        onChange={e => handleSemChange(e, index)}
+                      />
+                      <Input
+                        isReadOnly
+                        size='lg'
+                        name='cgpa'
+                        type='number'
+                        label={`C.G.P.A.`}
+                        value={sem.cgpa}
+                        color={getCGPAColor(parseFloat(sem.cgpa))}
+                      />
+                    </div>
+                  )
+                })}
               </div>
               <Button
                 color='secondary'
