49eb61165bbd88b5821b96f42d0ff5120b0f64d0
Mohammed Hamza <211220035@nitdelhi.ac.in>
2025-01-18 19:05:38 +0530

    Analytics added

--- src/utils/calcCGPA.jsx
+++ src/utils/calcCGPA.jsx
@@ -1,20 +1,20 @@
-function calcCGPA(semesters) {
-  let weightedSgpaSum = 0;
-  let creditsSum = 0;
+function calcCGPA (semesters) {
+  let weightedSgpaSum = 0
+  let creditsSum = 0
 
   return semesters.map((sem, index) => {
-    const sgpa = parseFloat(sem.sgpa);
-    const credits = parseFloat(sem.credits);
+    const sgpa = parseFloat(sem.sgpa)
+    const credits = parseFloat(sem.credits)
 
     if (!isNaN(sgpa) && !isNaN(credits)) {
-      weightedSgpaSum += sgpa * credits;
-      creditsSum += credits;
+      weightedSgpaSum += sgpa * credits
+      creditsSum += credits
     }
 
-    const cgpa = (sgpa && credits) ? (weightedSgpaSum / creditsSum).toFixed(2) : '';
-    return { ...sem, cgpa };
-  });
+    const cgpa =
+      sgpa && credits ? (weightedSgpaSum / creditsSum).toFixed(2) : ''
+    return { ...sem, cgpa }
+  })
 }
 
-export default calcCGPA;
-
+export default calcCGPA
